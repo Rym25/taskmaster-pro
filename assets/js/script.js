e79a6bec80dbff13186f9315fd6a5e4ff8b394bb
Ryely <ryely.moore@outlook.com>
2021-05-25 17:25:58 -0600

    add custom styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -220,15 +220,23 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event) {
     console.log("activate",this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
     console.log("deactivate",this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
     console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event) {
     console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   update: function(event) {
     // Array to store the task data in
@@ -283,4 +291,10 @@ $("#trash").droppable({
 loadTasks();
 
 $("#modalDueDate").datepicker({minDate: 1});
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
 
